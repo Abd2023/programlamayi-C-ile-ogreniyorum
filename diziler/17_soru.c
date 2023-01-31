a3a12ccbd6b6591f54935d3c08cc0e25af486a3f
Abd2023 <114013715+Abd2023@users.noreply.github.com>
2023-01-31 14:17:02 +0300

    Create 17_soru.c

---
+++ diziler/17_soru.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+//11.17
+int main()
+{
+    int rastgeleDizi[50];
+    int elemanSayi=0;
+    printf("Eleman Sayisini Giriniz : ");
+    scanf("%d", &elemanSayi);
+    srand(time(NULL));
+    for(int i=0; i<elemanSayi; i++)
+    {
+        rastgeleDizi[i]= 1 + rand()%100;
+    }
+    printf("Otomatik olusturan dizi : ");
+    for(int i=0; i<elemanSayi; i++)
+    {
+        printf("%d, ", rastgeleDizi[i]);
+    }
+    int rastgeleDiziKopy[elemanSayi];
+    for(int i=0; i<elemanSayi; i++)
+        rastgeleDiziKopy[i]=rastgeleDizi[i];
+    int temp=0;
+    int niyaz;
+    int elemanSayiKopy={0};
+    elemanSayiKopy=elemanSayi;
+    do
+    {
+        niyaz=0;
+        for(int i=0; i<elemanSayiKopy; i++)
+         {
+             if(rastgeleDizi[i]<rastgeleDizi[i+1])
+             {
+                 temp=rastgeleDizi[i];
+                 rastgeleDizi[i]=rastgeleDizi[i+1];
+                 rastgeleDizi[i+1]=temp;
+                 niyaz=1;
+             }
+         }
+         elemanSayiKopy--;
+    } while (niyaz!=0);
+    printf("\nDizinin Siralnmis Hali : ");
+    for(int i=0; i<elemanSayi; i++)
+        printf("%d, ", rastgeleDizi[i]);
+    int count=0;
+    for(int i=0; i<elemanSayi; i++)
+    {
+        if(rastgeleDizi[i]!=rastgeleDiziKopy[i])
+            count++;
+    }
+    printf("\nYeri degisenlerin sayisi  :  %d", count);
+
+    return 0;
+}
